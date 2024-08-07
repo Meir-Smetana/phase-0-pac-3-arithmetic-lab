ceb8d77dd1926b793e53df0263372a0ea01c8958
chaim meir smetana <meirsmetana@gmail.com>
2024-08-07 13:49:18 -0400

    this was hard

---
+++ index.js
@@ -0,0 +1,59 @@
+function add(a, b){
+return a+b;
+}
+
+function subtract(a, b){
+return a-b;
+}
+function divide(a, b){
+return a/b;
+}
+const a = 1;
+const b = 2;
+function multiply(a, b){
+return a*b;
+}
+
+
+function increment(n){
+    return n + 1;
+}
+function decrement(n){
+    return n - 1;
+}
+
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
+
+
+
+
+
+
+
+
+
+let n = 5;
+
+
+
+
+
+
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
